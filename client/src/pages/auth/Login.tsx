838b1da1bc89c557856fa3b5e1f36323035e6871
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-27 21:58:22 +0800

    Created Login Component

---
+++ client/src/pages/auth/Login.tsx
@@ -0,0 +1,67 @@
+import React, {  useState } from "react";
+import { Link } from "react-router-dom";
+import { UserValueTypes } from "../../types/types";
+
+const Login = () => {
+
+    const [ values, setValues ] = useState<UserValueTypes>(
+        {
+            username: "",
+            password: "",
+        }
+    );
+
+    const controlledInputs = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+
+        const name: string = e.target.name;
+        const value: string  = e.target.value;
+
+        setValues( ( p ) => ( { ...p, [ name ]: value } ) )
+
+    }
+
+    return (
+        <div className="wrapper login">
+
+            <div className="about-wrapper">
+                <div className="title-wrapper"> <p> Devs</p> </div>
+                <div className="content-wrapper">
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, facere cupiditate obcaecati cumque, voluptates repudiandae, repellendus assumenda laborum eaque quibusdam architecto ratione dicta ipsum reiciendis a harum pariatur illum adipisci? </p>
+                </div>
+            </div>
+
+            <div className="form-wrapper">
+                <form>
+                    <div className="title-wrapper">
+                        <p> Login </p>
+                    </div>
+                    <div className="label-wrapper">
+                        <label htmlFor="Username">
+                            <p> Username </p>
+                            <input id="Username" type="text" name="username" onChange={ controlledInputs } placeholder="Username..." value={ values?.username} />
+                        </label>
+                    </div>
+                    <div className="label-wrapper">
+                        <label htmlFor="Password">
+                            <p> Password </p>
+                            <div className="password-wrapper">
+                                <input id="Password" type="text" name="password" onChange={ controlledInputs } placeholder="Password ..." value={ values?.password} />
+                                <button type="button"> show </button>
+                            </div>
+                        </label>
+                    </div>
+                    <div className="buttons-wrapper">
+                        <button type="button"> Login </button>
+                    </div>
+                    <div className="extra-wrapper">
+                        <p> Dont have an account yet? <Link to={'/signup'}> Signup </Link> </p>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Login;
