1cdd7f17aea81ce822ce72e44270317ef5897528
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 12:58:10 +0800

    Routes for Index was set

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
+import Index from "./pages";
 
 const App = () => {
 
   return (
     <Routes>
+      <Route path="/" element={ <Index/> } />
       <Route path="/login" element={ <Login/> } />
       <Route path="/signup" element={ <Signup/> } />
     </Routes>
