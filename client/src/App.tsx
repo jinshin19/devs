416cf4ffcdb236ae582d23c7f0ecfe7fa8f7c89b
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 20:33:26 +0800

    Routes for update was set

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
 import Index from "./pages";
+import Update from "./pages/Update";
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={ <Index/> } />
       <Route path="/login" element={ <Login/> } />
       <Route path="/signup" element={ <Signup/> } />
+      <Route path="/update/:id" element={ <Update/> } />
     </Routes>
   )
 
