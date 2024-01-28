cfeaad5195a8ff731a6452b39554b37bad0af821
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 12:57:25 +0800

    Added Settings  component

---
+++ client/src/components/Settings.tsx
@@ -0,0 +1,14 @@
+import { faGear } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const Settings = () => {
+
+    return (
+        <button type="button">
+            <FontAwesomeIcon icon={ faGear }/>
+        </button>
+    )
+  
+}
+
+export default Settings
