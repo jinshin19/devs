b578ea08503d23b053faf3d83ba10ccd9f2ffd63
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 12:56:27 +0800

    Added footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+
+  return (
+    <footer>
+        <small> Alrights Reserved Devs 2023 | created by <Link to="/"> joshdev19 </Link> </small>
+    </footer>
+  )
+
+}
+
+export default Footer
