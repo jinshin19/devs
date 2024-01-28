b816e3c8d4331c918cbe5bd98c1d13fe5139c895
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 12:56:07 +0800

    Added devs container component

---
+++ client/src/components/DevsContainer.tsx
@@ -0,0 +1,13 @@
+import Devs from "./Devs"
+
+const DevsContainer = () => {
+  return (
+    <div className="devs-container">
+        <Devs/>
+        <Devs/>
+        <Devs/>
+    </div>
+  )
+}
+
+export default DevsContainer
