ef1028c97e9a7db87515bbcebf11fc787a79f97f
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 12:58:29 +0800

    Imported some components

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import DevsContainer from "../components/DevsContainer";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+
+const Index = () => {
+
+    return (
+        <div className="wrapper index-wrapper">
+
+            <Nav/>
+            <DevsContainer/>
+            <Footer/>
+
+        </div>
+    )
+
+}
+
+export default Index;
