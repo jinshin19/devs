014dbd58aeb73a0877260964927e4486080ced48
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 20:32:53 +0800

    Added Update component

---
+++ client/src/pages/Update.tsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer"
+import Nav from "../components/Nav"
+import UpdateComponent from "../components/UpdateComponent"
+
+const Update = () => {
+
+    return (
+        <div className="wrapper update-wrapper">
+            <Nav/>
+            <UpdateComponent/>
+            <Footer/>
+        </div>
+    )
+  
+}
+
+export default Update
