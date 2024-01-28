ae71744714009a3bf38645ae27674e8fb2bcacba
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-01-28 20:34:01 +0800

    Made some changes in the types

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,6 +1,7 @@
 export type UserValueTypes = {
     username: string;
     firstname?: string;
+    middlename?: string;
     lastname?: string;
     password: string;
     confirm_password?: string;
