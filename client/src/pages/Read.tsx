33dc62dc5d59293c47c862836255a20456e2daf1
joshua@doableservices.com <joshua@doableservices.com>
2024-02-03 21:34:07 +0800

    Created Read.tsx

---
+++ client/src/pages/Read.tsx
@@ -0,0 +1,18 @@
+import Footer from "../components/Footer"
+import Nav from "../components/Nav"
+import ReadComponent from "../components/ReadComponent"
+
+const Read = () => {
+  
+    return (
+    
+        <div className="wrapper read-wrapper">
+            <Nav/>
+            <ReadComponent/>
+            <Footer/>
+        </div>
+  
+    )
+}
+
+export default Read
