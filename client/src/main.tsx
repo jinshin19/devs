5a6c6078a3ebff7ed477c7d482fd09abe338ef05
joshua@doableservices.com <joshua@doableservices.com>
2024-02-09 23:15:28 +0800

    Imported query client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
   </BrowserRouter>,
 )
