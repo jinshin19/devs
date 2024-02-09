535c6de39e49b394761a74a9579f24a162c02949
joshua@doableservices.com <joshua@doableservices.com>
2024-02-09 23:24:52 +0800

    Made changes in the database

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,23 +1,24 @@
-import mysql from 'mysql2/promise';
+import mysql, { QueryOptions } from 'mysql2/promise';
+import { StatementTypes } from '../types/types';
 
-const pool = mysql.createPool(
-    {
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASS,
-        database: process.env.MYSQL_DATABASE,
-        host: process.env.MYSQL_HOST,
-        waitForConnections: true,
-        connectionLimit: 5,
-        queueLimit: 0
-    }
-);
+export const performDatabaseConnection = async ( stmts: StatementTypes & QueryOptions, values: any ) => {
 
-export const performDatabaseConnection = async () => {
+    const pool = mysql.createPool(
+        {
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASS,
+            database: process.env.MYSQL_DATABASE,
+            host: process.env.MYSQL_HOST,
+            waitForConnections: true,
+            connectionLimit: 5,
+            queueLimit: 0
+        }
+    );
 
     try {
         
         const connection = await pool.getConnection();
-        const [ rows, fields ] = await connection.query('');
+        const [ rows, fields ] = await connection.query(stmts, values);
         connection.release();
         return rows;
 
@@ -25,7 +26,9 @@ export const performDatabaseConnection = async () => {
 
         if ( error ) {
 
-            console.log("Error", error);
+            if ( error ) return {
+                error
+            }
             
         }
     }
