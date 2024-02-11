fdc01af88b7c61bf8229b3861b5c8442c7f210d8
joshua@doableservices.com <joshua@doableservices.com>
2024-02-11 13:19:17 +0800

    Made changes in the getAlldevs statement

--- server/sql-statements/statments.ts
+++ server/sql-statements/statments.ts
@@ -1,7 +1,7 @@
 import { StatementTypes } from "../types/types";
 
 export const statements: StatementTypes = {
-    getAllDevs: "select * from devs",
+    getAllDevs: "select dev_id, firstname, middlename, lastname from devs",
     getDevsByID: "select * from devs where dev_id = ?",
     createDev: "insert into devs ( dev_id, username, firstname, middlename, lastname, stacks, links, password  ) values ( ? )",
     updateDev: "update devs set username = ?, firstname = ?, middlename = ?, lastname = ?, stacks = ?, links = ?, password = ?",
