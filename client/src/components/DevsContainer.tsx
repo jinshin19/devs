ef392a6f8e807e854a1a6bd56ca983515a7e08b1
joshua@doableservices.com <joshua@doableservices.com>
2024-02-11 13:20:35 +0800

    Imported query

--- client/src/components/DevsContainer.tsx
+++ client/src/components/DevsContainer.tsx
@@ -1,11 +1,14 @@
+import { useGetDevsQuery } from "../services/queries"
+import { DevTypes } from "../types/types";
 import Devs from "./Devs"
 
 const DevsContainer = () => {
+
+  const { data } = useGetDevsQuery();
+
   return (
     <div className="devs-container">
-        <Devs/>
-        <Devs/>
-        <Devs/>
+        { data && data?.map( ( d: DevTypes) => <Devs key={d?.dev_id} { ...d } /> ) }
     </div>
   )
 }
