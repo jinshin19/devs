3ec30cd0f7adc40caaffaefb3c126148e47ad518
joshua@doableservices.com <joshua@doableservices.com>
2024-02-11 13:07:00 +0800

    Made new statemnets

--- server/sql-statements/statments.ts
+++ server/sql-statements/statments.ts
@@ -5,5 +5,5 @@ export const statements: StatementTypes = {
     getDevsByID: "select * from devs where dev_id = ?",
     createDev: "insert into devs ( dev_id, username, firstname, middlename, lastname, stacks, links, password  ) values ( ? )",
     updateDev: "update devs set username = ?, firstname = ?, middlename = ?, lastname = ?, stacks = ?, links = ?, password = ?",
-    deleteDev: "delete from devs where dev_id = ?"
+    deleteDev: "delete from devs where id = ?"
 }
