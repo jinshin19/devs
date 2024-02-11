f506b4a43a79a44d41b3613e34561a98df643d62
joshua@doableservices.com <joshua@doableservices.com>
2024-02-11 13:23:27 +0800

    Made changes

--- client/src/components/Devs.tsx
+++ client/src/components/Devs.tsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom'
 import imagePlaceHolder from '../assets/image-placeholder.png'
+import { DevTypes } from '../types/types'
 
-const Devs = () => {
+const Devs = ( { dev_id, firstname, middlename, lastname }: DevTypes ) => {
 
   return (
-    <Link to="/" className="devs">
+    <Link to={`/read/${dev_id}`} className="devs">
         <div className="image-wrapper">
             <img src={ imagePlaceHolder} alt="" />
         </div>
         <div className="content-wrapper">
-            <p> Joshua Philip Unilongo</p>
+            <p> {`${firstname} ${middlename} ${lastname}`} </p>
         </div>
     </Link>
   )
