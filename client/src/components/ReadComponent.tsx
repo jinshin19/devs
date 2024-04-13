1914625a5e7e8598b60dbd58c362bc7f60a35ecd
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-13 16:10:29 +0800

    feat/firstname, middlename, lastname & bio was set

--- client/src/components/ReadComponent.tsx
+++ client/src/components/ReadComponent.tsx
@@ -1,47 +1,59 @@
 import imagePlaceholder from "../assets/image-placeholder.png";
+import { DevTypes } from "../types/types";
 
+const ReadComponent = ({
+  id,
+  username,
+  firstname,
+  middlename,
+  lastname,
+  bio,
+  stacks,
+  links,
+}: DevTypes) => {
+  console.log("bio", bio);
+  console.log(firstname);
+  console.log(middlename);
+  console.log(lastname);
 
-const ReadComponent = () => {
-
-    return (
-        <div className="read-container">
-            <div className="read-content-wrapper">
-                <div className="image-wrapper">
-                    <img src={imagePlaceholder} alt="image" />
-                </div>
-                <div className="name-wrapper">
-                    <p> Joshua Philip Concepcion Unilongo </p>
-                </div>
-                <div className="bio-wrapper">
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus ducimus architecto, reprehenderit dignissimos, quidem dolorem vel corrupti repellat exercitationem soluta iure odit quo inventore consequatur dolor numquam, quibusdam praesentium? </p>
-                </div>
-                <div className="stacks-wrapper">
-                    <div className="title-wrapper">
-                        <p> Stacks </p>
-                    </div>
-                    <ul>
-                        <li> HTML </li>
-                        <li> CSS </li>
-                        <li> JAVASCRIPT </li>
-                    </ul>
-                </div>
-                <div className="links-wrapper">
-                    <div className="title-wrapper">
-                        <p> Social Links </p>
-                    </div>
-                    <ul>
-                        <li>
-                            <a href=""> Facebook </a>
-                        </li>
-                        <li>
-                            <a href=""> Google </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="read-container">
+      <div className="read-content-wrapper">
+        <div className="image-wrapper">
+          <img src={imagePlaceholder} alt="image" />
         </div>
-    )
+        <div className="name-wrapper">
+          <p>{`${firstname} ${middlename} ${lastname}`}</p>
+        </div>
+        <div className="bio-wrapper">
+          <p>{`"${bio}"`}</p>
+        </div>
+        <div className="stacks-wrapper">
+          <div className="title-wrapper">
+            <p> Stacks </p>
+          </div>
+          <ul>
+            <li> HTML </li>
+            <li> CSS </li>
+            <li> JAVASCRIPT </li>
+          </ul>
+        </div>
+        <div className="links-wrapper">
+          <div className="title-wrapper">
+            <p> Social Links </p>
+          </div>
+          <ul>
+            <li>
+              <a href=""> Facebook </a>
+            </li>
+            <li>
+              <a href=""> Google </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default ReadComponent
+export default ReadComponent;
