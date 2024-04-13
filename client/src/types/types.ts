1c75b4c06f14b862f7fef1f68c1da9f118a6b2c5
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-13 16:04:32 +0800

    remove ID types

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,19 +1,19 @@
 export type UserValueTypes = {
-    username: string;
-    firstname?: string;
-    middlename?: string;
-    lastname?: string;
-    password: string;
-    confirm_password?: string;
-}
+  username: string;
+  firstname?: string;
+  middlename?: string;
+  lastname?: string;
+  password: string;
+  confirm_password?: string;
+};
 
 export type DevTypes = {
-    id?: number | string;
-    dev_id: string;
-    username?: string;
-    firstname: string;
-    middlename: string;
-    lastname: string;
-    stacks?: [];
-    links?: [];
-}
+  id?: number | string;
+  username?: string;
+  firstname: string;
+  middlename: string;
+  lastname: string;
+  bio: string;
+  stacks?: [];
+  links?: [];
+};
