ad2ec555a0f7827667ec854d29910e684f038c17
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-13 16:09:31 +0800

    feat/spread operator was use instead & minor changes

--- client/src/pages/Read.tsx
+++ client/src/pages/Read.tsx
@@ -1,18 +1,20 @@
-import Footer from "../components/Footer"
-import Nav from "../components/Nav"
-import ReadComponent from "../components/ReadComponent"
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+import ReadComponent from "../components/ReadComponent";
+import { Params, useParams } from "react-router-dom";
+import { useGetDevByIDQuery } from "../services/queries";
 
 const Read = () => {
-  
-    return (
-    
-        <div className="wrapper read-wrapper">
-            <Nav/>
-            <ReadComponent/>
-            <Footer/>
-        </div>
-  
-    )
-}
+  const { id }: Params = useParams();
+  const { data } = useGetDevByIDQuery(id!);
+  console.log(data);
+  return (
+    <div className="wrapper read-wrapper">
+      <Nav />
+      <ReadComponent {...data?.data[0]} />
+      <Footer />
+    </div>
+  );
+};
 
-export default Read
+export default Read;
