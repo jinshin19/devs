507b3014cf8bf5273352b8499ef35486660e7441
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-14 23:06:07 +0800

    feat/made new types

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,19 +1,32 @@
-export type UserValueTypes = {
-  username: string;
-  firstname?: string;
-  middlename?: string;
-  lastname?: string;
-  password: string;
-  confirm_password?: string;
+type DevTypes = {
+  firstname: string | null;
+  lastname: string | null;
 };
 
-export type DevTypes = {
-  id?: number | string;
-  username?: string;
-  firstname: string;
-  middlename: string;
-  lastname: string;
-  bio: string;
-  stacks?: [];
-  links?: [];
+export type LoginDevTypes = {
+  username: string;
+  password: string;
+}
+
+export type SignupDevTypes = DevTypes & {
+  username: string;
+  password: string | null;
+  confirm_password: string | null;
+};
+
+export type GetAllDevDataTypes = DevTypes & {
+  id: string;
+  middlename: string | null;
+};
+
+export type GetDevDataTypes = GetAllDevDataTypes & {
+  bio: string | null;
+  stacks: string | null;
+  links: string | null;
+};
+
+export type AddORUpdateDevDataTypes = GetDevDataTypes & {
+  username: string | null;
+  password: string | null;
+  confirm_password: string | null;
 };
