bc013d80d7ba6ba272ba3d79f3416ee47051cbdc
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-14 23:01:53 +0800

    feat/use state instead rather than passing ref inputs that causes the register to undefined

--- client/src/components/ShowPassword.tsx
+++ client/src/components/ShowPassword.tsx
@@ -1,54 +1,24 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { Dispatch, SetStateAction } from "react";
 
 interface PropType {
-    password: any;
-    confirm_password?: any;
+  show: boolean;
+  setShow: Dispatch<SetStateAction<boolean>>;
 }
 
-const ShowPassword = ( { password, confirm_password }: PropType) => {
-
-    const [ show, setShow ] = useState(false)
-
-    const buttonHandler = ()  => {
-
-        setShow( !show )
-
-        if ( !show ) {
-
-            password.current.type = "text"
-            if ( confirm_password !== undefined ) {
-                confirm_password.current.type = "text"
-            } else {
-                null
-            }
-
-        }
-        else {
-
-            password.current.type = "password"
-            if ( confirm_password !== undefined ) {
-                confirm_password.current.type = "password"
-            } else {
-                null
-            }
-
-        }
-    }
-
-    return (
-        <div className="show-password">
-            <button type='button' onClick={ buttonHandler }>
-                {
-                    show ? 
-                    <FontAwesomeIcon icon={faEye} /> :
-                    <FontAwesomeIcon icon={faEyeSlash} />
-                }
-            </button>
-        </div>
-    )
-
-}
+const ShowPassword = ({ show, setShow }: PropType) => {
+  return (
+    <div className="show-password">
+      <button type="button" onClick={() => setShow(!show)!}>
+        {show ? (
+          <FontAwesomeIcon icon={faEye} />
+        ) : (
+          <FontAwesomeIcon icon={faEyeSlash} />
+        )}
+      </button>
+    </div>
+  );
+};
 
 export default ShowPassword;
