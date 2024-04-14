a7a6e258beb9343b74c658605838dd85c3de0c43
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-14 23:09:58 +0800

    feat/set hook form, refs was removed and leave a note

--- client/src/pages/auth/Signup.tsx
+++ client/src/pages/auth/Signup.tsx
@@ -1,85 +1,97 @@
-import React, { useRef, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { UserValueTypes } from "../../types/types";
+import { SignupDevTypes } from "../../types/types";
 import ShowPassword from "../../components/ShowPassword";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const Signup = () => {
+  const [show, setShow] = useState(false);
 
-    const [ values, setValues ] = useState<UserValueTypes>(
-        {
-            username: "",
-            firstname: "",
-            lastname: "",
-            password: "",
-            confirm_password: ""
-        }
-    );
+  const { register, handleSubmit } = useForm<SignupDevTypes>();
 
-    const controlledInputs = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+  const signUpHandler: SubmitHandler<SignupDevTypes> = (data) => {
+    console.log(data);
+    Modify the existing mutation in the services folder
+    change the url to '/devs/auth/signin'
+  };
 
-        const name: string = e.target.name;
-        const value: string  = e.target.value;
-
-        setValues( ( p ) => ( { ...p, [ name ]: value } ) )
-
-    }
-
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const confirmPasswordRef = useRef<HTMLInputElement>(null);
-
-    return (
-        <div className="wrapper signup">
-
-            <div className="form-wrapper">
-                <form>
-                    <div className="title-wrapper">
-                        <p> Signup </p>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Username">
-                            <p> Username </p>
-                            <input id="Username" type="text" name="username" onChange={ controlledInputs } placeholder="Username..." value={ values?.username} />
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Firstname">
-                            <p> Firstname </p>
-                            <input id="Firstname" type="text" name="firstname" onChange={ controlledInputs } placeholder="Firstname..." value={ values?.firstname} />
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Lastname">
-                            <p> Lastname </p>
-                            <input id="Lastname" type="text" name="lastname" onChange={ controlledInputs } placeholder="Lastname..." value={ values?.lastname} />
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Password">
-                            <p> Password </p>
-                            <div className="password-wrapper">
-                                <input id="Password" type="password" name="password" onChange={ controlledInputs } placeholder="Password ..." value={ values?.password} ref={ passwordRef } />
-                                <ShowPassword password={ passwordRef } confirm_password={ confirmPasswordRef } />
-                            </div>
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="ConfirmPassword">
-                            <p> Confirm Password </p>
-                            <input id="ConfirmPassword" type="password" name="confirm_password" onChange={ controlledInputs } placeholder="Confirm Password ..." value={ values?.confirm_password} ref={ confirmPasswordRef } />
-                        </label>
-                    </div>
-                    <div className="buttons-wrapper">
-                        <button type="button"> Signup </button>
-                    </div>
-                    <div className="extra-wrapper">
-                        <p> Already have an account? <Link to={'/login'}> login </Link> </p>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-    )
-
-}
+  return (
+    <div className="wrapper signup">
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit(signUpHandler)}>
+          <div className="title-wrapper">
+            <p> Signup </p>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Username">
+              <p> Username </p>
+              <input
+                id="Username"
+                type="text"
+                placeholder="Username..."
+                {...register("username")}
+              />
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Firstname">
+              <p> Firstname </p>
+              <input
+                id="Firstname"
+                type="text"
+                placeholder="Firstname..."
+                {...register("firstname")}
+              />
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Lastname">
+              <p> Lastname </p>
+              <input
+                id="Lastname"
+                type="text"
+                placeholder="Lastname..."
+                {...register("lastname")}
+              />
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Password">
+              <p> Password </p>
+              <div className="password-wrapper">
+                <input
+                  id="Password"
+                  type={show ? "text" : "password"}
+                  placeholder="Password ..."
+                  {...register("password")}
+                />
+                <ShowPassword show={show} setShow={setShow} />
+              </div>
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="ConfirmPassword">
+              <p> Confirm Password </p>
+              <input
+                id="ConfirmPassword"
+                type={show ? "text" : "password"}
+                placeholder="Confirm Password ..."
+                {...register("confirm_password")}
+              />
+            </label>
+          </div>
+          <div className="buttons-wrapper">
+            <button type="submit"> Signup </button>
+          </div>
+          <div className="extra-wrapper">
+            <p>
+              Already have an account? <Link to={"/login"}> login </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
