239062356b81b515dc1d70a3a198fee93e579827
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-15 20:43:50 +0800

    feat/added signup in the dev endpoint

--- server/routes/devs_routes.ts
+++ server/routes/devs_routes.ts
@@ -11,6 +11,6 @@ export const devsRoutes = Router();
 
 devsRoutes.get("/devs", getAllDevs);
 devsRoutes.get("/devs/:id", getDevByID);
-devsRoutes.post("/devs", createDev);
+devsRoutes.post("/devs/signup", createDev);
 devsRoutes.put("/devs/:id", updateDevByID);
 devsRoutes.delete("/devs/:id", deleteDevByID);
