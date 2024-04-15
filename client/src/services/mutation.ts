3ea34a7894edeb01609bd675b0930599e12ee44b
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-15 22:50:59 +0800

    feat/Made use of the toast and navigate user if success

--- client/src/services/mutation.ts
+++ client/src/services/mutation.ts
@@ -1,17 +1,19 @@
-import { useMutation } from "@tanstack/react-query"
-import { createDevAPI } from "./api"
-import { toast } from 'react-toastify'
+import { useMutation } from "@tanstack/react-query";
+import { createDevAPI } from "./api";
+import { toast } from "react-toastify";
+import { SignupDevTypes } from "../types/types";
+import { useNavigate } from "react-router-dom";
 
 export const useCreateDev = () => {
-
-    return useMutation(
-        {
-            mutationFn: ( data ) => createDevAPI( data ),
-            onSuccess: () => toast.success("Sign in successfully"),
-            onSettled: ( _, data, variables ) => {
-                if ( error ) return console.log( error )
-            }
-        }
-    )
-
-}
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: (data: SignupDevTypes) => createDevAPI(data),
+    onSuccess: () => {
+      toast.success("Sign in successfully");
+      navigate("/");
+    },
+    onSettled: (_, error) => {
+      if (error) return toast.error(error?.response.data.message);
+    },
+  });
+};
