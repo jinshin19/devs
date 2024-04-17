2f6f5ef119f9d448b247f78d1c61ce914bf204b8
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-17 21:36:22 +0800

    feat/added useEffect to automatically close the settings nav

--- client/src/components/Settings.tsx
+++ client/src/components/Settings.tsx
@@ -1,18 +1,21 @@
-import { faGear } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { settingAtom } from "../atoms/use_toggler"
-import { useAtom } from 'jotai'
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { settingAtom } from "../atoms/use_toggler";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 const Settings = () => {
+  const [show, setShow] = useAtom(settingAtom);
 
-    const [ show, setShow ] = useAtom(settingAtom);
+  useEffect(() => {
+    setShow(false);
+  }, [setShow]);
 
-    return (
-        <button type="button" onClick={() => setShow(!show)}>
-            <FontAwesomeIcon icon={ faGear }/>
-        </button>
-    )
-  
-}
+  return (
+    <button type="button" onClick={() => setShow(!show)}>
+      <FontAwesomeIcon icon={faGear} />
+    </button>
+  );
+};
 
-export default Settings
+export default Settings;
