45bbe67ab8dd9c5da24d1b4ec6fe6ece0e3380b3
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-17 15:47:05 +0800

    feat/updated updatePage

--- client/src/components/UpdateComponent.tsx
+++ client/src/components/UpdateComponent.tsx
@@ -1,172 +1,198 @@
-import React, { useRef, useState } from "react";
-import { UserValueTypes } from "../types/types";
-import ShowPassowrd from "./ShowPassword";
-import imagePlaceHolder from '../assets/image-placeholder.png';
+import { useEffect } from "react";
+import imagePlaceHolder from "../assets/image-placeholder.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
-
-interface LinksType {
-    title: string;
-    link: string;
-}
+import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
+import { AddORUpdateDevDataTypes } from "../types/types";
+import { useParams } from "react-router-dom";
+import { useUpdateDev } from "../services/mutation";
+import { axiosInstance } from "../services/api";
 
 const UpdateComponent = () => {
+  const { id } = useParams();
+  const updateDev = useUpdateDev();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm<AddORUpdateDevDataTypes>();
+  const { fields, remove, append } = useFieldArray({
+    control,
+    name: "links",
+  });
 
-    const [ values, setValues ] = useState<UserValueTypes>(
-        {
-            username: "",
-            firstname: "",
-            middlename: "",
-            lastname: "",
-            password: "",
-            confirm_password: ""
+  const addLinkHandler = () => append({ title: "", link: "" });
+  const deleteLinkHandler = (index: number) => remove(index);
+
+  const updateHandler: SubmitHandler<AddORUpdateDevDataTypes> = (data) => {
+    updateDev.mutate(data);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axiosInstance.get(`/devs/${id}`);
+      const { data } = response;
+      setValue("bio", data?.data.bio);
+      setValue("username", data?.data.username);
+      setValue("firstname", data?.data.firstname);
+      setValue("middlename", data?.data.middlename);
+      setValue("lastname", data?.data.lastname);
+      setValue("stacks", data?.data.stacks);
+      data?.data.links?.map(
+        ({ title, link }: { title: string; link: string }) => {
+          setValue("links.title", title);
+          setValue("links.link", link);
         }
-    );
+      );
+    };
+    fetchData();
+  }, [id, setValue]);
 
-    const [ links, setLinks ] = useState<LinksType[]>(
-        [
-            {
-                title: "",
-                link: ""
-            }
-        ]
-    );
-
-    const controlledInputs = ( e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> ) => {
-
-        const name: string = e.target.name;
-        const value: string  = e.target.value;
-
-        setValues( ( p ) => ( { ...p, [ name ]: value } ) )
-
-    }
-
-    const controlledLinkInputs = ( e: React.ChangeEvent<HTMLInputElement>, index: number ) => {
-
-        const name: string = e.target.name;
-        const value: string  = e.target.value;
-        
-        setLinks( (p) => {
-
-            const updateLinks = [...p];
-            updateLinks[index] = {
-                ...updateLinks[index],
-                [name]: value
-            };
-            return updateLinks;
-            
-        })
-    }
-
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const confirmPasswordRef = useRef<HTMLInputElement>(null);
-
-    const deleteLinkHandler = ( index: number ) => ( links?.length > 1 ) && setLinks( ( p ) => p.filter( (_, i) => i !== index) )
-
-    const addLinkHandler = () => {
-        setLinks( (p) => [ ...p, { title: '', link: '' } ] )
-    }
-
-    return (
-        <div className="update-container">
-            <div className="form-wrapper">
-                <form>
-                    <div className="title-wrapper">
-                        <p> Update </p>
-                    </div>
-                    <div className="image-wrapper">
-                        <div className="image-container">
-                            <img src={ imagePlaceHolder } alt="" />
-                        </div>
-                        <label htmlFor="File">
-                            <p> File: downloads/ </p>
-                            <p> Upload File </p>
-                            <input id="File" type="file" name="file" onChange={ controlledInputs } placeholder="File..." hidden />
-                        </label>
-                    </div>
-                    <div className="label-wrapper bio-wrapper">
-                        <label htmlFor="Bio">
-                            <p> Bio </p>
-                            <textarea id="Bio" name="confirm_password" onChange={ controlledInputs } placeholder="Bio..." value={ values?.confirm_password} ></textarea>
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Username">
-                            <p> Username </p>
-                            <input id="Username" type="text" name="username" onChange={ controlledInputs } placeholder="Username..." value={ values?.username} />
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Firstname">
-                            <p> Firstname </p>
-                            <input id="Firstname" type="text" name="firstname" onChange={ controlledInputs } placeholder="Firstname..." value={ values?.firstname} />
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Firstname">
-                            <p> Middlename </p>
-                            <input id="Middlename" type="text" name="middlename" onChange={ controlledInputs } placeholder="Middlename..." value={ values?.middlename} />
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Lastname">
-                            <p> Lastname </p>
-                            <input id="Lastname" type="text" name="lastname" onChange={ controlledInputs } placeholder="Lastname..." value={ values?.lastname} />
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="Password">
-                            <p> Password </p>
-                            <div className="password-wrapper">
-                                <input id="Password" type="password" name="password" onChange={ controlledInputs } placeholder="Password..." value={ values?.password} ref={ passwordRef } />
-                                <ShowPassowrd password={ passwordRef } confirm_password={ confirmPasswordRef } />
-                            </div>
-                        </label>
-                    </div>
-                    <div className="label-wrapper">
-                        <label htmlFor="ConfirmPassword">
-                            <p> Confirm Password </p>
-                            <input id="ConfirmPassword" type="password" name="confirm_password" onChange={ controlledInputs } placeholder="Confirm Password..." value={ values?.confirm_password} ref={ confirmPasswordRef } />
-                        </label>
-                    </div>
-                    <h2> Stacks </h2>
-                    <div className="label-wrapper stack-wrapper">
-                        <label htmlFor="Stacks">
-                            <p> Stacks </p>
-                            <textarea id="Stacks" name="stacks" onChange={ controlledInputs } placeholder="Enter stack and add comma ( e.g: html, css, js )" value={ values?.confirm_password} ></textarea>
-                        </label>
-                    </div>
-                    <h2> Social Links </h2>
-                    <div className="label-wrapper links-wrapper">
-                        <div className="input-wrapper">
-                            {
-                                links && links?.map( ( l, index ) => {
-                                    return (
-                                        <div className="inputs" key={ index }>
-                                            <input type="text" name="title" onChange={ e => controlledLinkInputs( e, index ) } placeholder="Title..." value={ l?.title } />
-                                            <input type="text" name="link" onChange={ e => controlledLinkInputs( e, index )  } placeholder="Link..." value={ l?.link } />
-                                            <button type="button" onClick={ () => deleteLinkHandler( index) }>
-                                                <FontAwesomeIcon icon={ faTrash }/>
-                                            </button>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                        <div className="add-wrapper">
-                            <button type="button" onClick={ addLinkHandler } >
-                                <p> Add New <FontAwesomeIcon icon={ faPlus }/> </p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="buttons-wrapper">
-                        <button type="button"> Update </button>
-                    </div>
-                </form>
+  return (
+    <div className="update-container">
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit(updateHandler)}>
+          <input type="hidden" value={id} />
+          <div className="title-wrapper">
+            <p> Update </p>
+          </div>
+          <div className="image-wrapper">
+            <div className="image-container">
+              <img src={imagePlaceHolder} alt="" />
             </div>
-        </div>
-    )
+            <label htmlFor="File">
+              <p> File: downloads/ </p>
+              <p> Upload File </p>
+              <input
+                id="File"
+                type="file"
+                name="file"
+                placeholder="File..."
+                hidden
+              />
+            </label>
+          </div>
+          <div className="label-wrapper bio-wrapper">
+            <label htmlFor="Bio">
+              <p> Bio </p>
+              <textarea
+                id="Bio"
+                placeholder="Bio..."
+                {...register("bio")}
+              ></textarea>
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Username">
+              {errors.username ? (
+                <p className="required"> Username is required </p>
+              ) : (
+                <p> Username </p>
+              )}
+              <input
+                id="Username"
+                type="text"
+                placeholder="Username..."
+                {...register("username", { required: true })}
+              />
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Firstname">
+              {errors.firstname ? (
+                <p className="required">Firstname is required</p>
+              ) : (
+                <p>Firstname</p>
+              )}
+              <input
+                id="Firstname"
+                type="text"
+                placeholder="Firstname..."
+                {...register("firstname", { required: true })}
+              />
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Firstname">
+              <p> Middlename </p>
+              <input
+                id="Middlename"
+                type="text"
+                placeholder="Middlename..."
+                {...register("middlename")}
+              />
+            </label>
+          </div>
+          <div className="label-wrapper">
+            <label htmlFor="Lastname">
+              {errors.lastname ? (
+                <p className="required"> Lastname is required </p>
+              ) : (
+                <p> Lastname </p>
+              )}
+              <input
+                id="Lastname"
+                type="text"
+                placeholder="Lastname..."
+                {...register("lastname", { required: true })}
+              />
+            </label>
+          </div>
+          <h2> Stacks </h2>
+          <div className="label-wrapper stack-wrapper">
+            <label htmlFor="Stacks">
+              <p> Stacks </p>
+              <textarea
+                id="Stacks"
+                placeholder="Enter stack and add comma ( e.g: html, css, js )"
+                {...register("stacks")}
+              ></textarea>
+            </label>
+          </div>
+          <h2> Social Links </h2>
+          <div className="label-wrapper links-wrapper">
+            <div className="input-wrapper">
+              {fields &&
+                fields?.map((item, index) => (
+                  <div className="inputs" key={item?.id}>
+                    <input
+                      type="text"
+                      placeholder="Title..."
+                      {...register(`links.${index}.title`)}
+                      defaultValue={item?.title}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Link..."
+                      {...register(`links.${index}.link`)}
+                      defaultValue={item?.link}
+                    />
+                    <button
+                      type="button"
+                      onClick={() => deleteLinkHandler(index)}
+                    >
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </div>
+                ))}
+            </div>
+            <div className="add-wrapper">
+              <button type="button" onClick={addLinkHandler}>
+                <p>
+                  Add New <FontAwesomeIcon icon={faPlus} />{" "}
+                </p>
+              </button>
+            </div>
+          </div>
+          <div className="buttons-wrapper">
+            <button type="submit"> Update </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default UpdateComponent
+export default UpdateComponent;
