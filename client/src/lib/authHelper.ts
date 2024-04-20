338338f1e8e8f1b54057a3b22b728f1615dd01ef
joshdev19 <151938289+joshdev19@users.noreply.github.com>
2024-04-20 11:29:43 +0800

    feat/updated note

--- client/src/lib/authHelper.ts
+++ client/src/lib/authHelper.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from "../services/api";
 
 /** Note to myself:
- *  Be aware of this, token was set to 1min only, if the token expires then you / users will be redirected to login page
+ *  Be aware of this, token was set to 15min only, if the token expires then you / users will be redirected to login page
  */
 
 export const isAuthenticated = async () => {
