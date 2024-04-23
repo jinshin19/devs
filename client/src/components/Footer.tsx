02d692a02a1d2b53324360e50174ef23a70ada1c
unilongojoshua94@gmail.com <unilongojoshua94@gmail.com>
2024-04-23 15:37:54 +0800

    feat/changed the creator

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-
   return (
     <footer>
-        <small> Alrights Reserved Devs 2023 | created by <Link to="/"> joshdev19 </Link> </small>
+      <small>
+        {" "}
+        Alrights Reserved Devs 2023 | created by{" "}
+        <Link to="https://github.com/jinshin19" target="_blank">
+          {" "}
+          jinshin19{" "}
+        </Link>{" "}
+      </small>
     </footer>
-  )
+  );
+};
 
-}
-
-export default Footer
+export default Footer;
